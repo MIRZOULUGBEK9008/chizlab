332a8b1f8a06ebf4ddff23114ad79333168ad354
mirzoulugbekkhudoyberdiyev9008 <ulugbekkhudoyberdiyev9008@gmail.com>
2025-05-21 22:51:48 +0500

    Add cta notification

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,58 @@
 "use client";
-import { Button } from "./ui/button";
-import { Cross1Icon, HamburgerMenuIcon } from "@radix-ui/react-icons";
+import { Button, buttonVariants } from "./ui/button";
+import {
+  ChatBubbleIcon,
+  Cross1Icon,
+  DashIcon,
+  HamburgerMenuIcon,
+} from "@radix-ui/react-icons";
 import TestMode from "./TestMode";
 import { useAppStore } from "@/lib/zustand";
 import { LogoBlackImg } from "@/assets";
 import Link from "next/link";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import SearchMaterials from "./SearchMaterials";
+import { toast } from "sonner";
 
 function Header() {
   const { mobileNavbar, setMobileNavbar } = useAppStore();
   function handleClick() {
     setMobileNavbar();
   }
+
+  // CTA
+  useEffect(() => {
+    const id = setTimeout(() => {
+      toast(
+        <div className="flex flex-col">
+          <h3 className="font-medium text-base">Fikr qoldiring!</h3>
+          <p className="text-muted-foreground mb-3">
+            chizlab.uz haqida qoldirilgan har bir fikr biz uchun qadrli
+          </p>
+          <a
+            className={`${buttonVariants({
+              variant: "default",
+            })} !bg-expensive-green ml-auto`}
+            href="https://forms.gle/C9onVT5n25aic9i38"
+            target="_blank"
+          >
+            <ChatBubbleIcon />
+            Fikr qoldirish
+          </a>
+        </div>,
+        {
+          closeButton: true,
+          duration: 10000,
+          position: "bottom-right",
+          className: "!w-[400px] cta-notification",
+        }
+      );
+    }, 7000);
+
+    return () => {
+      clearTimeout(id);
+    };
+  }, []);
 
   return (
     <div>
