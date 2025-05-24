41073a1cae9e5c165fcbf7b10730a39f51d901b6
mirzoulugbekkhudoyberdiyev9008 <ulugbekkhudoyberdiyev9008@gmail.com>
2025-05-24 10:41:54 +0500

    Add GA

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -4,33 +4,17 @@ import Header from "@/components/Header";
 import { Desktop, Mobile } from "@/components/Navbars";
 import Footer from "@/components/Footer";
 import { Toaster } from "sonner";
+import Script from "next/script";
+import { GA_MEASUREMENT_ID } from "@/lib/gtag"; // GA ID ni import qilamiz
 
 const gilroyFont = localFont({
   src: [
-    {
-      path: "../assets/fonts/Gilroy-Black.woff",
-      weight: "900",
-    },
-    {
-      path: "../assets/fonts/Gilroy-SemiBold.woff",
-      weight: "600",
-    },
-    {
-      path: "../assets/fonts/Gilroy-Medium.woff",
-      weight: "500",
-    },
-    {
-      path: "../assets/fonts/Gilroy-Regular.woff",
-      weight: "400",
-    },
-    {
-      path: "../assets/fonts/Gilroy-Light.woff",
-      weight: "300",
-    },
-    {
-      path: "../assets/fonts/Gilroy-Thin.woff",
-      weight: "100",
-    },
+    { path: "../assets/fonts/Gilroy-Black.woff", weight: "900" },
+    { path: "../assets/fonts/Gilroy-SemiBold.woff", weight: "600" },
+    { path: "../assets/fonts/Gilroy-Medium.woff", weight: "500" },
+    { path: "../assets/fonts/Gilroy-Regular.woff", weight: "400" },
+    { path: "../assets/fonts/Gilroy-Light.woff", weight: "300" },
+    { path: "../assets/fonts/Gilroy-Thin.woff", weight: "100" },
   ],
 });
 
@@ -38,7 +22,6 @@ export const metadata = {
   title: "Bosh sahifa",
   description:
     "Chizmachilik va dizayn sohasidagi O'zbekistondagi ilk platforma.",
-
   openGraph: {
     title: "Chizlab loyihasi",
     description:
@@ -54,7 +37,6 @@ export const metadata = {
       },
     ],
   },
-
   alternates: {
     canonical: "https://www.chizlab.uz",
   },
@@ -63,7 +45,22 @@ export const metadata = {
 export default function layout({ children }) {
   return (
     <html className="h-full" lang="uz">
-      <head></head>
+      <head>
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_MEASUREMENT_ID}', {
+              page_path: window.location.pathname,
+            });
+          `}
+        </Script>
+      </head>
       <body
         className={`${gilroyFont.className} antialiased flex flex-col h-full bg-body bg-cover`}
       >
